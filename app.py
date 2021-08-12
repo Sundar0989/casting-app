97bcc4a9716bb112d32e4726c4381a616a3c3a31
Sundar Krishnan <sundar@Sundars-MacBook-Pro.local>
2021-08-11 20:50:25 -0400

    your comment3

--- app.py
+++ app.py
@@ -223,8 +223,7 @@ def create_app(test_config=None):
   return app
 
 # create the app
-APP = create_app()
-
+app = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
