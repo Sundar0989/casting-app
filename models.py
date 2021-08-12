9aa099c5a0046d12dba77a35dd5b65b8281b68e0
Sundar Krishnan <sundar@Sundars-MacBook-Pro.local>
2021-08-12 16:24:45 -0400

    py code style issues fixed9

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-database_path = os.environ.get('SQLALCHEMY_DATABASE_URI')
+database_path = os.environ.get('DATABASE_URL')
 
 if database_path == '':
     database_path = 'postgres://ynnpueixqcogzi:20bf6125162888bc438b6a2ee909d3d145e1c36a250c05c464618f7c7bcddcd4@ec2-35-174-56-18.compute-1.amazonaws.com:5432/dnkpt83fikbov'
