dc0d9be6ca6fd53d17c87c4bf25370e2c40c8b03
Sundar Krishnan <sundar@Sundars-MacBook-Pro.local>
2021-08-12 15:00:26 -0400

    py code style issues fixed7

--- manage.py
+++ manage.py
@@ -6,10 +6,10 @@ import os
 
 app = create_app()
 
-database_path = os.environ.get('SQLALCHEMY_DATABASE_URI')
-if database_path == '':
-    database_path = 'postgres://ynnpueixqcogzi:20bf6125162888bc438b6a2ee909d3d145e1c36a250c05c464618f7c7bcddcd4@ec2-35-174-56-18.compute-1.amazonaws.com:5432/dnkpt83fikbov'
-app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+# database_path = os.environ.get('SQLALCHEMY_DATABASE_URI')
+# if database_path == '':
+#     database_path = 'postgres://ynnpueixqcogzi:20bf6125162888bc438b6a2ee909d3d145e1c36a250c05c464618f7c7bcddcd4@ec2-35-174-56-18.compute-1.amazonaws.com:5432/dnkpt83fikbov'
+# app.config["SQLALCHEMY_DATABASE_URI"] = database_path
 
 migrate = Migrate(app, db)
 manager = Manager(app)
