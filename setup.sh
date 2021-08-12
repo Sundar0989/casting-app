70cb3304ba45162fa55543e20337e586798cfb8d
Sundar Krishnan <sundar@Sundars-MacBook-Pro.local>
2021-08-11 21:02:30 -0400

    your comment4

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
 
+export SQLALCHEMY_DATABASE_URI='postgres://ynnpueixqcogzi:20bf6125162888bc438b6a2ee909d3d145e1c36a250c05c464618f7c7bcddcd4@ec2-35-174-56-18.compute-1.amazonaws.com:5432/dnkpt83fikbov'
