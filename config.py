1375b7e1323b663c2bc581bd6da4bbf7e4af5712
Sundar Krishnan <sundar@Sundars-MacBook-Pro.local>
2021-08-11 21:12:15 -0400

    your comment5

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 import os 
 
-database_path = os.getenv('SQLALCHEMY_DATABASE_URI')
-
-if database_path == '':
-    database_path = "postgres://ynnpueixqcogzi:20bf6125162888bc438b6a2ee909d3d145e1c36a250c05c464618f7c7bcddcd4@ec2-35-174-56-18.compute-1.amazonaws.com:5432/dnkpt83fikbov"
+database_path = "postgres://ynnpueixqcogzi:20bf6125162888bc438b6a2ee909d3d145e1c36a250c05c464618f7c7bcddcd4@ec2-35-174-56-18.compute-1.amazonaws.com:5432/dnkpt83fikbov"
 
